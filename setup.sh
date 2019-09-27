5ad8a9ca928f6f329bf49094fa0dabfec695e5a4
noriyuki-sasagawa <sasanori.0126@gmail.com>
2019-09-27 16:19:58 +0900

    shellのエスケープとか修正

--- setup.sh
+++ setup.sh
@@ -2,16 +2,19 @@
 cd $HOME
 mkdir -p development/go/src
 brew install --HEAD goenv
-echo `# golang` >>  ~/.bash_profile
-echo `export PATH="$HOME/.goenv/bin:$PATH"` >> ~/.bash_profile
+echo "# golang" >>  ~/.bash_profile
+echo export PATH='$HOME/.goenv/bin:$PATH' >> ~/.bash_profile
 echo eval "$(goenv init -)" >> ~/.bash_profile
 source ~/.bash_profile
-goenv install 1.13
-goenv global 1.13
-echo `export GOPATH=$HOME/development/go` >> ~/.bash_profile
-echo `export PATH=$PATH:$GOPATH/bin` >> ~/.bash_profile
-echo `export PATH=$PATH:/usr/local/go/bin` >> ~/.bash_profile
+goenv install 1.13.0
+goenv global 1.13.0
+goenv rehash
+echo export GOPATH='$HOME/development/go' >> ~/.bash_profile
+echo export PATH='$PATH:$GOPATH/bin' >> ~/.bash_profile
+echo export PATH='$PATH:/usr/local/go/bin' >> ~/.bash_profile
 source ~/.bash_profile
+brew install dep
+brew upgrade dep
 cd $GOPATH/src
 git clone git@github.com:noriyuki-sasagawa/go_example.git
 dep ensure
