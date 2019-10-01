3e62debcbddb09dc91c11d22d603a55ebf7d1cbd
noriyuki-sasagawa <sasanori.0126@gmail.com>
2019-10-01 17:57:31 +0900

    パッケージの設定変えてみる

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $HOME
-mkdir -p development/go/src/github.com/me/
+mkdir -p development/go/src
 brew install --HEAD goenv
 echo "# golang" >>  ~/.bash_profile
 echo export PATH='$HOME/.goenv/bin:$PATH' >> ~/.bash_profile
@@ -14,9 +14,9 @@ echo export GOPATH='$HOME/development/go' >> ~/.bash_profile
 echo export PATH='$PATH:$GOPATH/bin' >> ~/.bash_profile
 echo export PATH='$PATH:/usr/local/go/bin' >> ~/.bash_profile
 source ~/.bash_profile
-cd $GOPATH/src/github.com/me/
+cd $GOPATH/src
 git clone git@github.com:noriyuki-sasagawa/go_example.git
 brew install dep
 brew upgrade dep
 dep ensure
-go run $GOPATH/src/github.com/me/go_example/main.go
+go run $GOPATH/src/go_example/main.go
