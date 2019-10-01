5af0b2c2014a071c4bce336b0338e131f378980a
noriyuki-sasagawa <sasanori.0126@gmail.com>
2019-10-01 17:17:13 +0900

    type修正

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ echo export GOPATH='$HOME/development/go' >> ~/.bash_profile
 echo export PATH='$PATH:$GOPATH/bin' >> ~/.bash_profile
 echo export PATH='$PATH:/usr/local/go/bin' >> ~/.bash_profile
 source ~/.bash_profile
-brew install dep
-brew upgrade dep
 cd $GOPATH/src/github.com/$USER/
 git clone git@github.com:noriyuki-sasagawa/go_example.git
+brew install dep
+brew upgrade dep
 dep ensure
 go run $GOPATH/src/github.com/$USER/go_example/main.go
